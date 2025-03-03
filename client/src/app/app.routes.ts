92281c06b9aa4b0d332030c132beb897642b31ab
Pierre-Adrien MARQUE <pierre-adrien.marque@institutsolacroup.com>
2025-03-03 09:47:42 +0100

    angular installation

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: LoginComponent},
+    {path:'/unauthorized', component: UnauthorizedComponent}
+];
---
+++ client/src/app/pages/profil-user/profil-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './profil-user.component.html',
+  styleUrl: './profil-user.component.css'
+})
+export class ProfilUserComponent {
+
+}
