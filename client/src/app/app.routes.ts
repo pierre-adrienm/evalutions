cc0d73c8e2457331eb83a7c4b2ab70f7949e5a06
Pierre-Adrien MARQUE <pierre-adrien.marque@institutsolacroup.com>
2025-03-03 10:22:17 +0100

    login page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AuthGuard } from './guard/auth.guard';
+import { UserComponent } from './pages/user/user.component';
+import { ProfilUserComponent } from './pages/profil-user/profil-user.component';
 
 export const routes: Routes = [
     {path:'', component: LoginComponent},
-    {path:'/unauthorized', component: UnauthorizedComponent}
+    {path:'admin/:id', component: AdminComponent, canActivate: [AuthGuard]},
+    {path:'user/:id', component: UserComponent, canActivate: [AuthGuard]},
+    {path:'profiluser/:id', component:ProfilUserComponent, canActivate: [AuthGuard]},
+    {path:'unauthorized', component: UnauthorizedComponent}
 ];
--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: LoginService,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.minLength(3), Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    localStorage.clear();
+    this.auth.logout().subscribe({
+      next: () => console.log('Utilisateur déconnecté'),
+      error: (err) => console.error('Erreur lors de la déconnexion:', err)
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.email?.value;
+      const password = this.password?.value;
+  
+      if (email && password) {
+        this.auth.login(email, password).subscribe({
+          next: (response: any) => { 
+            const { status, id_user } = response.user;
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('status', status);
+            localStorage.setItem('userId', id_user);
+  
+            this.router.navigateByUrl(`/${status}/${id_user}`);
+          },
+          error: (err: any) => { 
+            console.error('Login error:', err);
+          }
+        });
+      }
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
